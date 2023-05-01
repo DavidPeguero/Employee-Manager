8412c65b9fb8b5bd36f0bdd425dbf3e397f22dc8
David Peguero <david.peguero.cancel@gmail.com>
2023-04-30 21:34:04 -0400

    Added way to add new role

--- index.js
+++ index.js
@@ -4,52 +4,53 @@ const mySql = require('mysql2');
 
 const db = mySql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: 'rootroot',
-      database: 'employees_db'
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'employees_db'
     },
     console.log(`Connected to the employees_db database.`)
-  );
+);
 
 let possiblePrompts =
-    {
-        name: 'selection',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [
-            'View All Employees',
-            'View All Roles',
-            'View All Departments',
-            'Add Employee',
-            'Add Role',
-            'Add Department',
-            'Update Employee'
-        ],
-    }
+{
+    name: 'selection',
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: [
+        'View All Employees',
+        'View All Roles',
+        'View All Departments',
+        'Add Employee',
+        'Add Role',
+        'Add Department',
+        'Update Employee',
+        'Quit'
+    ],
+}
 
 let employeeCreationPrompts = [
-        {
-            name: 'employeeFirstName',
-            type: 'input',
-            message: "What is the employee's first name?",
-        },
-        {
-            name: 'employeeLastName',
-            type: 'input',
-            message: "What is the employee's last name?",
-        },
-        {
-            name: 'employeeRole',
-            type: 'input',
-            message: "What is the employee's role?",
-        },
-        {
-            name: 'employeeManager',
-            type: 'input',
-            message: "Who is the employee's manager?",
-        },
-    ]
+    {
+        name: 'employeeFirstName',
+        type: 'input',
+        message: "What is the employee's first name?",
+    },
+    {
+        name: 'employeeLastName',
+        type: 'input',
+        message: "What is the employee's last name?",
+    },
+    {
+        name: 'employeeRole',
+        type: 'input',
+        message: "What is the employee's role?",
+    },
+    {
+        name: 'employeeManager',
+        type: 'input',
+        message: "Who is the employee's manager?",
+    },
+]
 
 let roleCreationPrompts = [
     {
@@ -76,37 +77,102 @@ let departmentCreationPrompt = {
 }
 
 
-async function init(){
+async function init() {
     //Ask user what they would like to do
-    let task =  await askTask();
+    let task = await askTask();
     //Check for the option selected by user
-    switch (task){
-        case'View All Employees':
+    switch (task) {
+        case 'View All Employees':
+            showTable(['employees']);
+            break;
+        case 'View All Departments':
+            showTable(['departments']);
+            break;
+        case 'View All Roles':
+            showTable(['roles']);
+            break;
+        case 'Add Employee':
             console.log(task);
             break;
-        case'View All Departments':
+        case 'Add Role':
+            await addRole();
+            break;
+        case 'Add Department':
             console.log(task);
             break;
-        case'View All Roles':
+        case 'Update Employee':
             console.log(task);
-            break;
-        case'Add Employee':
-            console.log(task);
-            break;
-        case'Add Role':
-            console.log(task);
-            break;
-        case'Add Department':
-            console.log(task);
-            break;
-        case'Update Employee':
-            console.log(task);     
+        case 'Quit':
+            process.exit();
     }
 }
 
-async function askTask(){
+async function askTask() {
     let task = await inquirer.prompt(possiblePrompts);
     return task.selection;
 }
+
+function showTable(parameters) {
+    db.query('SELECT * FROM ??', parameters, (err, res) => {
+        console.table(res);
+    })
+}
+
+async function addRole() {
+    db.query('SELECT * FROM departments', async (err, res) => {
+        let deptNames = [];
+        let deptIds = []
+
+        //Extract the department names from the db
+        for(const dept of res){
+            deptNames.push(dept.name);
+            deptIds.push(dept.id);
+        }
+        
+        //Create prompts to ask based of department info
+        let roleCreationPrompts = [
+            {
+                name: 'roleName',
+                type: 'input',
+                message: "What is the the name of the role?",
+            },
+            {
+                name: 'roleSalary',
+                type: 'input',
+                message: "What is the salary of the role?",
+            },
+            {
+                name: 'roleDepartment',
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: deptNames
+            }
+        ]
+
+        //Ask prompts to the user
+        let roleAns = await inquirer.prompt(roleCreationPrompts);
+        
+        //Create values for the new role to add
+        let newRole = [roleAns.roleName, roleAns.roleSalary, deptIds[deptNames.indexOf(roleAns.roleDepartment)]];
+        
+        //Query db with info about new roles as a parameter
+        db.query('INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)', newRole, (err, res) => {
+            err ? console.error('Could not add employee') : console.log('Added Employee')
+        });
+    })
+    
+
+    // console.log(deptNames);
+    // console.log(deptIds);
+    
+    
+}
+
+// function addEmployee(){
+
+//     db.query('INSERT INTO employess (first_name, last_name, salary, department_id) VALUES (?)', postBody ,(err, results) =>{
+//         res.json({status : "Success", data : result});
+//     });
+// }
 
 init();
